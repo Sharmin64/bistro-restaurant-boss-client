1dd1bb66c8b357f994a5efa6c8a83daaa179ec0b
Sharmin64 <lizaeatsbanana@gmail.com>
2023-06-01 00:16:56 +0600

    json web token is implemented in usecart file

--- src/Pages/DashBoard/AllUsers/AllUsers.jsx
+++ src/Pages/DashBoard/AllUsers/AllUsers.jsx
@@ -1,7 +1,8 @@
 import {useQuery} from "@tanstack/react-query";
 import React from "react";
 import {Helmet} from "react-helmet-async";
-import {FaTrashAlt} from "react-icons/fa";
+import {FaTrashAlt, FaUserShield} from "react-icons/fa";
+import Swal from "sweetalert2";
 
 const AllUsers = () => {
   const {data: users = [], refetch} = useQuery(["users"], async () => {
@@ -9,6 +10,25 @@ const AllUsers = () => {
     return res.json();
   });
 
+  const handleMakeAdmin = (user) => {
+    fetch(`http://localhost:5001/users/admin/${user._id}`, {
+      method: "PATCH",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.modifiedCount) {
+          refetch();
+          Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: `${user.name} is an Admin Now!`,
+            showConfirmButton: false,
+            timer: 1500,
+          });
+        }
+      });
+  };
   const handleDelete = (user) => {
     console.log("buyyon deleted");
   };
@@ -38,7 +58,18 @@ const AllUsers = () => {
                 <th>{index + 1}</th>
                 <td>{user.name}</td>
                 <td>{user.email}</td>
-                <td>Blue</td>
+                <td>
+                  {user.role === "admin" ? (
+                    "admin"
+                  ) : (
+                    <button
+                      onClick={() => handleMakeAdmin(user)}
+                      className="btn btn-ghost btn-lg bg-amber-300 text-white"
+                    >
+                      <FaUserShield />
+                    </button>
+                  )}
+                </td>
                 <td>
                   <button
                     onClick={() => handleDelete(user)}
--- src/hooks/useCart.jsx
+++ src/hooks/useCart.jsx
@@ -3,12 +3,18 @@ import {useQuery} from "@tanstack/react-query";
 import {AuthContext} from "../Providers/AuthProvider";
 const useCart = () => {
   const {user} = useContext(AuthContext);
+  const token = localStorage.getItem("access-token");
 
   const {refetch, data: cart = []} = useQuery({
     queryKey: ["carts", user?.email],
     queryFn: async () => {
       const res = await fetch(
-        `http://localhost:5001/carts?email=${user?.email}`
+        `http://localhost:5001/carts?email=${user?.email}`,
+        {
+          headers: {
+            authorization: `bearer ${token}`,
+          },
+        }
       );
       return res.json();
     },
